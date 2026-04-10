58edb98c86e263d1f7dfde0e8078b74e7bc32790
Nocturne56 <loevan.discord@gmail.com>
2026-04-10 18:20:26 +0200

    modification du server.js
    /

--- server.js
+++ server.js
@@ -356,6 +356,7 @@ app.post("/api/deleteSanction", (req, res) => {
         res.json({ success: false });
     }
 });
-app.listen(3000, () => {
-    console.log("Serveur lancé sur http://localhost:3000");
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Serveur lancé sur le port " + PORT);
+  console.log("Serveur actif → https://crazy-fort.onrender.com/");
 });
